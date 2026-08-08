b4c66c665deb351765e9ff12f252637d6e7cc7a3
Belongie Lab <90206898+BelongieLab@users.noreply.github.com>
2026-08-08 22:05:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import KineticMatrix from './KineticMatrix';
+import Percentages from './Percentages';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/kinetic-matrix" element={<KineticMatrix />} />
+        <Route path="/percentages" element={<Percentages />} />
       </Routes>
     </BrowserRouter>
   );
